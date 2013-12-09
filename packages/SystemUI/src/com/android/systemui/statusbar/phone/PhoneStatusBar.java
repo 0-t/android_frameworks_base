20d85b257f86dffaf5320acf34ce0bb99659d5c8
Griffin Millender <griffinn.millender@gmail.com>
2013-12-09 08:18:54 -0600

    [1/3] fb: Change density on the fly
    Change-Id: I7ee16d32ee882fea22367f49f69f4da744f3aa45

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -213,6 +213,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     Point mCurrentDisplaySize = new Point();
     int mCurrUiThemeMode;
     int mCurrOrientation;
+    int mCurrentDensity;
     private float mHeadsUpVerticalOffset;
     private int[] mPilePosition = new int[2];
 
@@ -934,6 +935,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateDisplaySize();
 
         mCurrUiThemeMode = mContext.getResources().getConfiguration().uiThemeMode;
+        mCurrentDensity = mContext.getResources().getConfiguration().densityDpi;
 
         mLocationController = new LocationController(mContext); // will post a notification
         mBatteryController = new BatteryController(mContext);
@@ -3911,6 +3913,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (uiThemeMode != mCurrUiThemeMode) {
             mCurrUiThemeMode = uiThemeMode;
             recreateStatusBar(false);
+            return;
+        }
+
+        // detect density change
+        int density = res.getConfiguration().densityDpi;
+        if (density != mCurrentDensity) {
+            mCurrentDensity = density;
+            recreateStatusBar(true);
+            recreatePie(isPieEnabled());
             return;
         }
 
